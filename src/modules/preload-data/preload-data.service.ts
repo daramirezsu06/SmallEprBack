dca081aa8abf0f1028d6062ac1914ed16886a191
daramirezsu06 <daramirezsu06@gmail.com>
2025-02-28 00:27:30 -0500

    proloadData comented

--- src/modules/preload-data/preload-data.service.ts
+++ src/modules/preload-data/preload-data.service.ts
@@ -56,239 +56,239 @@ export class PreloadDataService {
   ) {}
 
   async preloadData() {
-    Cargar los datos de TypeProduct
-    const typeProductsData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'type_products.json'), 'utf-8'),
-    );
-    for (const typeData of typeProductsData) {
-      const typeProduct = this.typeProductRepository.create(typeData);
-      await this.typeProductRepository.save(typeProduct);
-    }
+    // Cargar los datos de TypeProduct
+    // const typeProductsData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'type_products.json'), 'utf-8'),
+    // );
+    // for (const typeData of typeProductsData) {
+    //   const typeProduct = this.typeProductRepository.create(typeData);
+    //   await this.typeProductRepository.save(typeProduct);
+    // }
 
-    // Cargar los MovementTypes
-    const movementTypesData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'movement_types.json'), 'utf-8'),
-    );
-    for (const movementTypeData of movementTypesData) {
-      const movementType = this.movementTypeRepository.create(movementTypeData);
-      await this.movementTypeRepository.save(movementType);
-    }
+    // // Cargar los MovementTypes
+    // const movementTypesData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'movement_types.json'), 'utf-8'),
+    // );
+    // for (const movementTypeData of movementTypesData) {
+    //   const movementType = this.movementTypeRepository.create(movementTypeData);
+    //   await this.movementTypeRepository.save(movementType);
+    // }
 
-    // Cargar los datos de SubTypeProduct
-    const subTypeProductsData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'sub_type_products.json'), 'utf-8'),
-    );
-    for (const subTypeData of subTypeProductsData) {
-      const { typeProductId, acronyms, name, description } = subTypeData;
-      const typeProduct = await this.typeProductRepository.findOne({
-        where: { id: typeProductId },
-      });
-      const subTypeProduct = this.subTypeProductRepository.create();
-      subTypeProduct.typeProduct = typeProduct;
-      subTypeProduct.acronyms = acronyms;
-      subTypeProduct.name = name;
-      subTypeProduct.description = description;
-      await this.subTypeProductRepository.save(subTypeProduct);
-    }
+    // // Cargar los datos de SubTypeProduct
+    // const subTypeProductsData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'sub_type_products.json'), 'utf-8'),
+    // );
+    // for (const subTypeData of subTypeProductsData) {
+    //   const { typeProductId, acronyms, name, description } = subTypeData;
+    //   const typeProduct = await this.typeProductRepository.findOne({
+    //     where: { id: typeProductId },
+    //   });
+    //   const subTypeProduct = this.subTypeProductRepository.create();
+    //   subTypeProduct.typeProduct = typeProduct;
+    //   subTypeProduct.acronyms = acronyms;
+    //   subTypeProduct.name = name;
+    //   subTypeProduct.description = description;
+    //   await this.subTypeProductRepository.save(subTypeProduct);
+    // }
 
-    // Cargar los datos de Unit
-    const unitsData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'units.json'), 'utf-8'),
-    );
-    for (const unitData of unitsData) {
-      const unit = this.unitRepository.create(unitData);
-      await this.unitRepository.save(unit);
-    }
+    // // Cargar los datos de Unit
+    // const unitsData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'units.json'), 'utf-8'),
+    // );
+    // for (const unitData of unitsData) {
+    //   const unit = this.unitRepository.create(unitData);
+    //   await this.unitRepository.save(unit);
+    // }
 
-    // Cargar los datos de Product
-    const productsData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'products.json'), 'utf-8'),
-    );
-    for (const productData of productsData) {
-      const {
-        typeProductId,
-        subTypeProductId,
-        name,
-        description,
-        unitId,
-        cost,
-        quantity,
-      } = productData;
-      const typeProduct = await this.typeProductRepository.findOne({
-        where: { id: typeProductId },
-      });
-      const subTypeProduct = await this.subTypeProductRepository.findOne({
-        where: { id: subTypeProductId },
-      });
-      const unit = await this.unitRepository.findOne({
-        where: { id: unitId },
-      });
-      const product = this.productRepository.create();
-      product.typeProduct = typeProduct;
-      product.subTypeProduct = subTypeProduct;
-      product.name = name;
-      product.description = description;
-      product.unit = unit;
-      await this.productRepository.save(product);
-      const inventory = this.inventoryRepository.create();
-      inventory.product = product;
-      inventory.quantity = quantity;
-      inventory.cost = cost;
-      await this.inventoryRepository.save(inventory);
-    }
-    // Cargar los datos de roles
-    const rolesData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'roles.json'), 'utf-8'),
-    );
-    for (const roleData of rolesData) {
-      const role = this.roleRepository.create(roleData);
-      await this.roleRepository.save(role);
-    }
+    // // Cargar los datos de Product
+    // const productsData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'products.json'), 'utf-8'),
+    // );
+    // for (const productData of productsData) {
+    //   const {
+    //     typeProductId,
+    //     subTypeProductId,
+    //     name,
+    //     description,
+    //     unitId,
+    //     cost,
+    //     quantity,
+    //   } = productData;
+    //   const typeProduct = await this.typeProductRepository.findOne({
+    //     where: { id: typeProductId },
+    //   });
+    //   const subTypeProduct = await this.subTypeProductRepository.findOne({
+    //     where: { id: subTypeProductId },
+    //   });
+    //   const unit = await this.unitRepository.findOne({
+    //     where: { id: unitId },
+    //   });
+    //   const product = this.productRepository.create();
+    //   product.typeProduct = typeProduct;
+    //   product.subTypeProduct = subTypeProduct;
+    //   product.name = name;
+    //   product.description = description;
+    //   product.unit = unit;
+    //   await this.productRepository.save(product);
+    //   const inventory = this.inventoryRepository.create();
+    //   inventory.product = product;
+    //   inventory.quantity = quantity;
+    //   inventory.cost = cost;
+    //   await this.inventoryRepository.save(inventory);
+    // }
+    // // Cargar los datos de roles
+    // const rolesData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'roles.json'), 'utf-8'),
+    // );
+    // for (const roleData of rolesData) {
+    //   const role = this.roleRepository.create(roleData);
+    //   await this.roleRepository.save(role);
+    // }
 
-    // Cargar los datos de usuarios
-    const usersData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'users.json'), 'utf-8'),
-    );
-    for (const userData of usersData) {
-      const { roleId, name, email, password } = userData;
-      const user = this.userRepository.create();
-      user.name = name;
-      user.email = email;
-      user.password = password;
-      const role = await this.roleRepository.findOne({ where: { id: roleId } });
-      user.role = role;
-      await this.userRepository.save(user);
-    }
+    // // Cargar los datos de usuarios
+    // const usersData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'users.json'), 'utf-8'),
+    // );
+    // for (const userData of usersData) {
+    //   const { roleId, name, email, password } = userData;
+    //   const user = this.userRepository.create();
+    //   user.name = name;
+    //   user.email = email;
+    //   user.password = password;
+    //   const role = await this.roleRepository.findOne({ where: { id: roleId } });
+    //   user.role = role;
+    //   await this.userRepository.save(user);
+    // }
 
-    // Cargar los TypeSeller
-    const typeSellersData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'type_sellers.json'), 'utf-8'),
-    );
-    for (const typeSellerData of typeSellersData) {
-      const typeSeller = this.typeSellerRepository.create(typeSellerData);
-      await this.typeSellerRepository.save(typeSeller);
-    }
+    // // Cargar los TypeSeller
+    // const typeSellersData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'type_sellers.json'), 'utf-8'),
+    // );
+    // for (const typeSellerData of typeSellersData) {
+    //   const typeSeller = this.typeSellerRepository.create(typeSellerData);
+    //   await this.typeSellerRepository.save(typeSeller);
+    // }
 
-    // Cargar los Sellers
-    const sellersData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'sellers.json'), 'utf-8'),
-    );
-    for (const sellerData of sellersData) {
-      const { typeSellerId, name, lastName, cedula, userId } = sellerData;
-      const typeSeller = await this.typeSellerRepository.findOne({
-        where: { id: typeSellerId },
-      });
-      const user = await this.userRepository.findOne({
-        where: { id: userId },
-      });
-      const seller = this.sellerRepository.create();
-      seller.typeSeller = typeSeller;
-      seller.name = name;
-      seller.lastName = lastName;
-      seller.cedula = cedula;
-      seller.user = user;
-      await this.sellerRepository.save(seller);
-    }
-    // Cargar los precios
-    const priceListsData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'price_lists.json'), 'utf-8'),
-    );
-    for (const priceListData of priceListsData) {
-      const { name, description, items } = priceListData;
-      const priceList = this.priceListRepository.create();
-      priceList.name = name;
-      priceList.description = description;
-      await this.priceListRepository.save(priceList);
-      for (const item of items) {
-        const priceListItem = this.priceListItemRepository.create();
-        priceListItem.price = item.price;
-        priceListItem.product = await this.productRepository.findOne({
-          where: { id: item.productId },
-        });
-        priceListItem.priceList = priceList;
-        await this.priceListItemRepository.save(priceListItem);
-      }
-      await this.priceListRepository.save(priceList);
-    }
+    // // Cargar los Sellers
+    // const sellersData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'sellers.json'), 'utf-8'),
+    // );
+    // for (const sellerData of sellersData) {
+    //   const { typeSellerId, name, lastName, cedula, userId } = sellerData;
+    //   const typeSeller = await this.typeSellerRepository.findOne({
+    //     where: { id: typeSellerId },
+    //   });
+    //   const user = await this.userRepository.findOne({
+    //     where: { id: userId },
+    //   });
+    //   const seller = this.sellerRepository.create();
+    //   seller.typeSeller = typeSeller;
+    //   seller.name = name;
+    //   seller.lastName = lastName;
+    //   seller.cedula = cedula;
+    //   seller.user = user;
+    //   await this.sellerRepository.save(seller);
+    // }
+    // // Cargar los precios
+    // const priceListsData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'price_lists.json'), 'utf-8'),
+    // );
+    // for (const priceListData of priceListsData) {
+    //   const { name, description, items } = priceListData;
+    //   const priceList = this.priceListRepository.create();
+    //   priceList.name = name;
+    //   priceList.description = description;
+    //   await this.priceListRepository.save(priceList);
+    //   for (const item of items) {
+    //     const priceListItem = this.priceListItemRepository.create();
+    //     priceListItem.price = item.price;
+    //     priceListItem.product = await this.productRepository.findOne({
+    //       where: { id: item.productId },
+    //     });
+    //     priceListItem.priceList = priceList;
+    //     await this.priceListItemRepository.save(priceListItem);
+    //   }
+    //   await this.priceListRepository.save(priceList);
+    // }
 
-    // Cargar los tipos de clientes
-    const typeCustomersData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'type_customer.json'), 'utf-8'),
-    );
-    for (const typeCustomerData of typeCustomersData) {
-      const { name, description } = typeCustomerData;
-      const typeCustomer = this.typeCustomerRepository.create();
-      typeCustomer.name = name;
-      typeCustomer.description = description;
-      await this.typeCustomerRepository.save(typeCustomer);
-    }
+    // // Cargar los tipos de clientes
+    // const typeCustomersData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'type_customer.json'), 'utf-8'),
+    // );
+    // for (const typeCustomerData of typeCustomersData) {
+    //   const { name, description } = typeCustomerData;
+    //   const typeCustomer = this.typeCustomerRepository.create();
+    //   typeCustomer.name = name;
+    //   typeCustomer.description = description;
+    //   await this.typeCustomerRepository.save(typeCustomer);
+    // }
 
-    // Cargar los clientes
-    const customersData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'customer.json'), 'utf-8'),
-    );
-    for (const customerData of customersData) {
-      const {
-        name,
-        address,
-        lat,
-        lon,
-        nit,
-        tel,
-        customerTypeId,
-        sellerId,
-        priceListId,
-      } = customerData;
-      const typeCustomer = await this.typeCustomerRepository.findOne({
-        where: { id: customerTypeId },
-      });
-      const seller = await this.sellerRepository.findOne({
-        where: { id: sellerId },
-      });
-      const priceList = await this.priceListRepository.findOne({
-        where: { id: priceListId },
-      });
-      const customer = this.customerRepository.create();
-      customer.typeCustomer = typeCustomer;
-      customer.name = name;
-      customer.address = address;
-      customer.lat = lat;
-      customer.lon = lon;
-      customer.nit = nit;
-      customer.tel = tel;
-      customer.typeCustomer = typeCustomer;
-      customer.seller = seller;
-      customer.priceList = priceList;
-      await this.customerRepository.save(customer);
-    }
+    // // Cargar los clientes
+    // const customersData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'customer.json'), 'utf-8'),
+    // );
+    // for (const customerData of customersData) {
+    //   const {
+    //     name,
+    //     address,
+    //     lat,
+    //     lon,
+    //     nit,
+    //     tel,
+    //     customerTypeId,
+    //     sellerId,
+    //     priceListId,
+    //   } = customerData;
+    //   const typeCustomer = await this.typeCustomerRepository.findOne({
+    //     where: { id: customerTypeId },
+    //   });
+    //   const seller = await this.sellerRepository.findOne({
+    //     where: { id: sellerId },
+    //   });
+    //   const priceList = await this.priceListRepository.findOne({
+    //     where: { id: priceListId },
+    //   });
+    //   const customer = this.customerRepository.create();
+    //   customer.typeCustomer = typeCustomer;
+    //   customer.name = name;
+    //   customer.address = address;
+    //   customer.lat = lat;
+    //   customer.lon = lon;
+    //   customer.nit = nit;
+    //   customer.tel = tel;
+    //   customer.typeCustomer = typeCustomer;
+    //   customer.seller = seller;
+    //   customer.priceList = priceList;
+    //   await this.customerRepository.save(customer);
+    // }
 
-    Cargar los Municipalities
-    const municipalitiesData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'municipalities.json'), 'utf-8'),
-    );
-    for (const municipalityData of municipalitiesData) {
-      const municipality = this.municipalityRepository.create(municipalityData);
-      await this.municipalityRepository.save(municipality);
-    }
+    // Cargar los Municipalities
+    // const municipalitiesData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'municipalities.json'), 'utf-8'),
+    // );
+    // for (const municipalityData of municipalitiesData) {
+    //   const municipality = this.municipalityRepository.create(municipalityData);
+    //   await this.municipalityRepository.save(municipality);
+    // }
 
-    // Cargar los Neighborhoods
-    const neighborhoodsData = JSON.parse(
-      fs.readFileSync(path.join(basePath, 'neighborhoods.json'), 'utf-8'),
-    );
-    for (const neighborhoodData of neighborhoodsData) {
-      const municipality = await this.municipalityRepository.findOne({
-        where: { id: neighborhoodData.municipalityId },
-      });
-      if (municipality) {
-        const neighborhood = this.neighborhoodRepository.create({
-          name: neighborhoodData.name,
-          lat: neighborhoodData.lat,
-          lon: neighborhoodData.lon,
-          municipality,
-        });
-        await this.neighborhoodRepository.save(neighborhood);
-      }
-    }
+    // // Cargar los Neighborhoods
+    // const neighborhoodsData = JSON.parse(
+    //   fs.readFileSync(path.join(basePath, 'neighborhoods.json'), 'utf-8'),
+    // );
+    // for (const neighborhoodData of neighborhoodsData) {
+    //   const municipality = await this.municipalityRepository.findOne({
+    //     where: { id: neighborhoodData.municipalityId },
+    //   });
+    //   if (municipality) {
+    //     const neighborhood = this.neighborhoodRepository.create({
+    //       name: neighborhoodData.name,
+    //       lat: neighborhoodData.lat,
+    //       lon: neighborhoodData.lon,
+    //       municipality,
+    //     });
+    //     await this.neighborhoodRepository.save(neighborhood);
+    //   }
+    // }
 
     console.log('Datos precargados correctamente');
   }
